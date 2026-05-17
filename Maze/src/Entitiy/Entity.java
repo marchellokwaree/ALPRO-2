16b9a0e62ce9ae2c0327a62122a9a5720935647c
Welly <wellychandra28@gmail.com>
2026-05-17 10:25:01 +0700

    Revert "Merge branch 'GAME.java' of https://github.com/marchellokwaree/ALPRO-2 into GAME.java"
    This reverts commit 03f8b2b8296559e40f0e4de2b919b1dea810a17d, reversing
    changes made to 0a05a5e8940ffdc7c7120a37cf1b2045413fe1c0.

--- Maze/src/Entitiy/Entity.java
+++ Maze/src/Entitiy/Entity.java
@@ -1,12 +1,11 @@
 package Entitiy;
 
 public class Entity {
-    public int x, y;
+    int x, y;
     int speed;
     public Entity(int x, int y, int speed) {
         this.x = x;
         this.y = y;
         this.speed = speed;
     }
-
 }
--- Maze/src/Main/GamePanel.java
+++ Maze/src/Main/GamePanel.java
@@ -1,123 +1,104 @@
 package Main;
 
-import Entitiy.Player;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+import Entitiy.Player;
+
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class GamePanel extends JPanel implements Runnable {
-    // Map data
-    char[][] map1 = new char[][]{
-        {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
-        {'1', 'S', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1'},
-        {'1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1'},
-        {'1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '1'},
-        {'1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '1', '0', '1', '1'},
-        {'1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1', '1'},
-        {'1', '1', '1', '0', '1', '0', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '1', '1'},
-        {'1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '1', '1'},
-        {'1', '0', '1', '1', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '0', '1', '0', '1', '1'},
-        {'1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1', '1'},
-        {'1', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '1'},
-        {'1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '1'},
-        {'1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '1', '1'},
-        {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '1', '1'},
-        {'1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '1', '0', '1', '1'},
-        {'1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1'},
-        {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', 'N'},
-        {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}
-    };
-    
-    final int tileSize = 32;
-    int maxScreenCol, maxScreenRow, screenWidth, screenHeight;
-    
+
+    char map1[][] = {
+            { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', },
+            { '1', 'S', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', },
+            { '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', },
+            { '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', },
+            { '1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '1', },
+            { '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '1', },
+            { '1', '1', '1', '0', '1', '0', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '1', '0', '1', },
+            { '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', },
+            { '1', '0', '1', '1', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '0', '0', '1', '0', '1', },
+            { '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1', },
+            { '1', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '1', },
+            { '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', },
+            { '1', '0', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1', '1', '1', '1', '1', '1', '0', '1', },
+            { '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '1', },
+            { '1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '1', },
+            { '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', },
+            { '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'G', '1', '0', '1', '0', '1', },
+            { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', } };
+
+    final int originalTileSize = 16;
+    final int scale = 2;
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = map1[0].length;
+    final int maxScreenRow = map1.length;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
     KeyHandler keyH = new KeyHandler();
+    Image Tanah;
+    Image Player;
+    Image NPC;
+    Image wallKananAtas;
+    Image wallKananBawah;
+    Image wallKiriAtas;
+    Image wallKiriBawah;
+    Image wall;
+    Image wallvertical;
+    Image wallhorizontal;
+    Image wallKiri, wallKanan, wallAtas, wallBawah;
     Thread gameThread;
     Player player;
-    
-    Image Tanah, wall, playerimg, ExitDoor;
+    int playerx, playery;
+    int endx, endy;
     BufferedImage bufferedImage;
 
-    public GamePanel() {
-        this.maxScreenCol = map1[0].length;
-        this.maxScreenRow = map1.length;
-        this.screenWidth = tileSize * maxScreenCol;
-        this.screenHeight = tileSize * maxScreenRow;
+    Image playerimg;
 
+    public GamePanel() {
         this.addKeyListener(keyH);
         this.setFocusable(true);
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.BLACK);
-        this.setDoubleBuffered(true);
+        this.setBackground(Color.BLUE);
+        this.setDoubleBuffered(true); // , sebuah teknik yang mencegah kedipan layar dengan merender grafik kompleks
+                                      // dalam buffer di luar layar sebelum menampilkannya.
 
-        loadAssets();
-
-        int startX = 0, startY = 0;
         for (int i = 0; i < maxScreenRow; i++) {
             for (int j = 0; j < maxScreenCol; j++) {
                 if (map1[i][j] == 'S') {
-                    startX = j * tileSize;
-                    startY = i * tileSize;
+                    playerx = j * tileSize;
+                    playery = i * tileSize;
                 }
-            }
-        }
-
-        int endX = 0, endY = 0;
-        for (int i = 0; i < maxScreenRow; i++) {
-            for (int j = 0; j < maxScreenCol; j++) {
                 if (map1[i][j] == 'N') {
-                    endX = j * tileSize;
-                    endY = i * tileSize;
+                    endx = j * tileSize;
+                    endy = i * tileSize;
                 }
             }
         }
-        
-        // Pastikan parameter Player sesuai dengan constructor baru di Player.java
-        player = new Player(this, keyH, playerimg, startX, startY);
-    }
 
-    public void loadAssets() {
         try {
-            // Loading Sprite Sheet
-            this.bufferedImage = ImageIO.read(getClass().getResourceAsStream("/Assets/ASSET/AnimationSheet.png"));
-            // Mengambil potongan gambar player (x, y, lebar, tinggi)
-            this.playerimg = bufferedImage.getSubimage(0, 0, 25, 25);
-            
-            // Loading Tiles
-            this.Tanah = loadImage("/Assets/lab_tileset_LITE/seperated/tile031.png");
-            this.wall = loadImage("/Assets/lab_tileset_LITE/seperated/tile066.png");
-            this.ExitDoor = loadImage("/Assets/lab_tileset_LITE/seperated/tile067.png");
+            System.out.println(
+                    "Loading bufferedImage from: " + getClass().getResource("/Assets/ASSET/AnimationSheet.png"));
+            bufferedImage = ImageIO.read(getClass().getResourceAsStream("/Assets/ASSET/AnimationSheet.png"));
+            System.out.println("BufferedImage loaded: " + (bufferedImage != null));
         } catch (Exception e) {
-            System.err.println("Error loading assets!");
+            System.out.println("Error loading AnimationSheet.png");
             e.printStackTrace();
         }
-    }
 
-    public Image loadImage(String path) {
-        try {
-            return new ImageIcon(getClass().getResource(path)).getImage();
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public int getTileSize() { return tileSize; }
-
-    public boolean collidesWithWall(int nextX, int nextY) {
-        int left = nextX / tileSize;
-        int right = (nextX + tileSize - 1) / tileSize;
-        int top = nextY / tileSize;
-        int bottom = (nextY + tileSize - 1) / tileSize;
-
-        if (left < 0 || right >= maxScreenCol || top < 0 || bottom >= maxScreenRow) return true;
-        return map1[top][left] == '1' || map1[top][right] == '1' || 
-               map1[bottom][left] == '1' || map1[bottom][right] == '1';
+        loadAssets();
+        player = new Player(this, keyH, playerimg, playerx, playery);
     }
 
     public void startGameThread() {
@@ -125,14 +106,36 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
+    public int getTileSize() {
+        return tileSize;
+    }
+
+    public boolean isWallTile(int col, int row) {
+        if (col < 0 || col >= maxScreenCol || row < 0 || row >= maxScreenRow) {
+            return true;
+        }
+        return map1[row][col] == '1';
+    }
+
+    public boolean collidesWithWall(int nextX, int nextY) {
+        int left = nextX / tileSize;
+        int right = (nextX + tileSize - 1) / tileSize;
+        int top = nextY / tileSize;
+        int bottom = (nextY + tileSize - 1) / tileSize;
+
+        return isWallTile(left, top) || isWallTile(right, top) ||
+                isWallTile(left, bottom) || isWallTile(right, bottom);
+    }
+
     @Override
     public void run() {
-        double drawInterval = 1000000000 / 60;
+        double drawInterval = 1000000000 / 60; // 60 FPS
         double delta = 0;
         long lastTime = System.nanoTime();
+        long currentTime;
 
         while (gameThread != null) {
-            long currentTime = System.nanoTime();
+            currentTime = System.nanoTime();
             delta += (currentTime - lastTime) / drawInterval;
             lastTime = currentTime;
 
@@ -141,51 +144,64 @@ public class GamePanel extends JPanel implements Runnable {
                 repaint();
                 delta--;
             }
-            checkWinCondition();
         }
-
-
     }
 
     public void update() {
-        if (player != null) player.update();
+        if (player != null) {
+            player.update();
+        }
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
+        // Gambar Map
         for (int i = 0; i < maxScreenRow; i++) {
             for (int j = 0; j < maxScreenCol; j++) {
-                if (Tanah != null) g2.drawImage(Tanah, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                if (map1[i][j] == '1' && wall != null) {
+                g2.drawImage(Tanah, j * tileSize, i * tileSize, tileSize, tileSize, null);
+                if (map1[i][j] == '1') {
                     g2.drawImage(wall, j * tileSize, i * tileSize, tileSize, tileSize, null);
-                }
-                if (map1[i][j] == 'N' && ExitDoor != null) {
-                    g2.drawImage(ExitDoor, j * tileSize, i * tileSize, tileSize, tileSize, null);
                 }
             }
         }
 
-        if (player != null) player.draw(g2);
+        // Gambar Player menggunakan method draw dari class Player
+        if (player != null) {
+            player.draw(g2);
+        }
+
         g2.dispose();
     }
 
-    protected void checkWinCondition() {
-        int playerTileX = player.x / tileSize;
-        int playerTileY = player.y / tileSize;
+    public void loadAssets() {
+        Tanah = loadImage("/Assets/lab_tileset_LITE/seperated/tile031.png");
 
-        if (map1[playerTileY][playerTileX] == 'N') {
-            // Tampilkan pesan kemenangan
-            System.out.println("Congratulations! You've reached the exit!");
-            WinGame();
+        NPC = loadImage("/Assets/lab_tileset_LITE/seperated/tile033.png");
+        wallKananAtas = loadImage("/Assets/lab_tileset_LITE/seperated/tile016.png");
+        wallKananBawah = loadImage("/Assets/lab_tileset_LITE/seperated/tile025.png");
+        wallKiriAtas = loadImage("/Assets/lab_tileset_LITE/seperated/tile015.png");
+        wallKiriBawah = loadImage("/Assets/lab_tileset_LITE/seperated/tile024.png");
+        wall = loadImage("/Assets/lab_tileset_LITE/seperated/tile066.png");
+        wallvertical = loadImage("/Assets/lab_tileset_LITE/seperated/tile039.png");
+        wallhorizontal = loadImage("/Assets/lab_tileset_LITE/seperated/tile040.png");
+        wallKiri = loadImage("/Assets/lab_tileset_LITE/seperated/tile055.png");
+        wallKanan = loadImage("/Assets/lab_tileset_LITE/seperated/tile054.png");
+        wallAtas = loadImage("/Assets/lab_tileset_LITE/seperated/tile068.png");
+        wallBawah = loadImage("/Assets/lab_tileset_LITE/seperated/tile041.png");
+        playerimg = bufferedImage.getSubimage(0, 0, 25, 25);
+    }
+
+    public Image loadImage(String path) {
+        Image img = null;
+        try {
+            img = new ImageIcon(getClass().getResource(path)).getImage();
+        } catch (Exception e) {
+            System.out.println("Error loading image: " + path);
+            e.printStackTrace();
         }
+        return img;
     }
 
-    protected void WinGame() {
-        // Implementasi logika kemenangan, matikan game loop, dan tampilkan pesan kemenangan
-        System.out.println("Congratulations! You've reached the exit!");
-        System.exit(0); // Keluar dari game
-    }
 }
